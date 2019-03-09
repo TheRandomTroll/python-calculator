4440770c9152f21e9e9a03f5711f5ac5841b2871
Mihail Gerginov <mi6o_g@windowslive.com>
2019-03-09 12:05:44 +0200

    Added functionality to calculator

--- calculator.py
+++ calculator.py
@@ -1,16 +1,12 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
+import trigonometry as trigonometry
 
 class Calculator:
     def __init__(self):
         print("Hello world")
 
     def execute(self, operation, a, b):
-        if operation == '+':
-            print(add(a, b))
+        if operation == 'tan':
+            print(trigonometry.tan(a))
         elif operation == '-':
             print(subtract(a, b))
 
