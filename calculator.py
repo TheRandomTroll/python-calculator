748912e9c215ce580b10d6a18d4d10055ca267aa
Mihail Gerginov <mi6o_g@windowslive.com>
2019-03-09 11:51:38 +0200

    Added basic arithmetics file

--- calculator.py
+++ calculator.py
@@ -1,29 +1,16 @@
-def add(a, b):
-    return a + b
+import basicarithmetic as basicarithmetic
 
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b
 
 class Calculator:
     def execute(self, operation, a, b):
         if operation == '+':
-            add(a, b)
+            basicarithmetic.add(a, b)
         elif operation == '-':
-            subtract(a, b)
+            basicarithmetic.subtract(a, b)
         elif operation == '*':
-            multiply(a, b)
+            basicarithmetic.multiply(a, b)
         elif(operation == '/'):
-            divide(a, b)
-
-
-
-
+            basicarithmetic.divide(a, b)
 
 c = Calculator()
 c.execute('+', 3, 4)
